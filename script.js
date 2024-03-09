4cf040a4e4f9b4ca8ca0f5aacb74d9e7623e73eb
jeffahn1 <140921206+jeffahn1@users.noreply.github.com>
2024-03-09 15:33:28 -0500

    updated keycodes

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const toggles = document.querySelectorAll('.faq-toggle')
+const insert = document.getElementById('insert')
 
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
+window.addEventListener('keydown', (event) => {
+  insert.innerHTML = `
+  <div class="key">
+  ${event.key === ' ' ? 'Space' : event.key} 
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>
+  `
 })
